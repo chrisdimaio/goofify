80725778a58526d4433379111165e2cb94f6025a
Chris DiMaio <chrisdimaio@gmail.com>
2021-02-23 22:36:17 -0500

    initial commit

---
+++ dumify-auth/lambda_function.py
@@ -0,0 +1,26 @@
+import json
+import logging
+import os
+import urllib3
+
+def lambda_handler(event, context):
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    
+    logger.info(event)
+    
+    endpoint = 'https://slack.com/api/oauth.v2.access'
+    code = event['queryStringParameters']['code']
+    client_id = os.environ['client_id']
+    client_secret = os.environ['client_secret']
+    redirect_uri = os.environ['redirect_uri']
+    
+    
+    http = urllib3.PoolManager()
+    r = http.request('GET', f'{endpoint}?code={code}&client_id={client_id}&client_secret={client_secret}&redirect_uri={redirect_uri}')
+    logger.info(r.data)
+    
+    return {
+        'statusCode': 200,
+        'body': 'Success'
+    }
---
+++ dumify/lambda_function.py
@@ -0,0 +1,70 @@
+import json
+import logging
+import os
+import urllib.parse
+import urllib3
+
+from base64 import b64decode
+from random import randint
+
+def lambda_handler(event, context):
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    
+    logger.info(json.dumps(event))
+    
+    
+    r = postmessage(getdata(event))
+    
+    logger.info(json.dumps(
+        {
+            'postmessage_response': {
+                'status': r.status,
+                'data': str(r.data)
+            }
+        }
+    ))
+    return {
+        'statusCode': 200,
+        'body': ''
+    }
+
+def dumify(dumstring):
+    dumified = ""
+    for c in dumstring:
+        if randint(0, 1) == 1:
+            dumified += c.upper()
+        else:
+            dumified += c.lower()
+    return dumified
+    
+def postmessage(data):
+    token = os.environ['bearer_token']
+    
+    http = urllib3.PoolManager()
+    r = http.request(
+        'POST', 
+        'https://slack.com/api/chat.postMessage', 
+        headers={
+            'Content-Type':'application/json',
+            'Authorization': f'Bearer {token}'
+        },
+        body=json.dumps({
+            'as_user': 'true',
+            'channel': data['channel_id'],
+            'username': data['user_name'],
+            'text': dumify(data['text']),
+        })
+    )
+    return r
+    
+def getdata(event):
+    body = event['body']
+    raw_data = urllib.parse.unquote_plus(str(b64decode(body).decode('utf-8')))
+    data = {}
+    pieces = raw_data.split("&")
+    for piece in pieces:
+        k,v = piece.split("=")
+        data[k] = v
+    return data
+    
