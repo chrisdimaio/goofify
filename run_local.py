76770a3ddea5fd72461fabd88678de1b4e5d1a71
Chris DiMaio <chrisdimaio@gmail.com>
2021-02-23 22:49:25 -0500

    created script for running dumify() locally

---
+++ run_local.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python3
+
+from dumify import lambda_function
+
+"""
+    Use this script to run dumify() locally.
+"""
+
+
+def main():
+    print(f'"{lambda_function.dumify("I want to make rockets and go to Mars")}" -Elon Musk')
+
+
+if __name__ == '__main__':
+    main()
